df07bd3eabeccb738e9a2b9b5ed5e93c048c4a69
shashi.rajge <shashikant.rajge@bakerhughes.com>
2023-02-01 17:23:17 +0530

    test2

---
+++ second.py
@@ -0,0 +1,14 @@
+n = int (input('enter the number of student'))
+A1 = list ()
+print (n)
+for i in range (1,n):
+    a = int (input ('enter the student age'))
+    print (i)
+    #a = A1[1]
+    print (A1[1])
+
+
+    
+
+
+
